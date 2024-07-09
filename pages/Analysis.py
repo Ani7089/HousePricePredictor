c5f1e1bb58052c976644a56a221b38c33955a636
Ani7089 <aniketdewangan31@gmail.com>
2024-07-09 14:43:46 +0530

    added few featues and resolved few problems in analysis.py file

--- pages/Analysis.py
+++ pages/Analysis.py
@@ -5,54 +5,52 @@ import pickle
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import seaborn as sns
+import squarify
 
-st.set_page_config(page_title="Analysis app")
+
+st.set_page_config(page_title="Analysis side")
 
 st.title('Analytics')
 
 new_df = pd.read_csv('E:\HousePricePredictor\Dataset\99Acres_gurgaon_hyderabad_Secunderabad_SelectedFeatures_with_fewChange.csv')
-feature_text = pickle.load(open('E:\HousePricePredictor\df.pkl','rb'))
+with open('E:\HousePricePredictor\df.pkl','rb') as file:
+    feature_text = pickle.load(file)
 
 
-group_df = new_df.groupby('LOCALITY').mean()[['PRICE','PRICE_SQFT','AREA','LATITUDE','LONGITUDE']]
+group_df = new_df.groupby('LOCALITY').mean(numeric_only=True)[['PRICE','PRICE_SQFT','AREA','LATITUDE','LONGITUDE']]
 
 st.header('Sector Price per Sqft Geomap')
-fig = px.scatter_mapbox(group_df, lat="LATITUDE", lon="LONGITUDE", color="PRICE_SQFT", size='AREA',
-                  color_continuous_scale=px.colors.cyclical.IceFire, zoom=10,
-                  mapbox_style="open-street-map",width=1200,height=700,hover_name=group_df.index)
 
-st.plotly_chart(fig,use_container_width=True)
+dense_area = group_df[group_df['PRICE_SQFT'] > group_df['PRICE_SQFT'].quantile(0.75)]  # Adjust the condition as needed
+center_lat = dense_area['LATITUDE'].mean()
+center_lon = dense_area['LONGITUDE'].mean()
+
+fig = px.scatter_mapbox(group_df, lat="LATITUDE", lon="LONGITUDE", color="PRICE_SQFT", size='AREA',
+                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10,
+                  mapbox_style="open-street-map", width=1200, height=700, hover_name=group_df.index,
+                  center=dict(lat=center_lat, lon=center_lon))
+
+st.plotly_chart(fig, use_container_width=True)
 
 st.header('Features Wordcloud')
 
 wordcloud = WordCloud(width = 800, height = 800,
                       background_color ='black',
                       stopwords = set(['s']),  # Any stopwords you'd like to exclude
-                      min_font_size = 10).generate(feature_text)
+                      min_font_size = 10).generate(str(feature_text))
 
-plt.figure(figsize = (8, 8), facecolor = None)
+fig = plt.figure(figsize = (8, 8), facecolor = None)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.tight_layout(pad = 0)
-st.pyplot()
+st.pyplot(fig)
 
 st.header('Area Vs Price')
 
 property_type = st.selectbox('Select Property Type', new_df['PROPERTY_TYPE'].unique().tolist())
 
-
-if property_type == 'Residential Apartment':
-    fig1 = px.scatter(new_df[new_df['PROPERTY_TYPE'] == 'Residential Apartment'], x="AREA", y="PRICE", color="BEDROOM_NUM", title="Area Vs Price")
-
-    st.plotly_chart(fig1, use_container_width=True)
-elif property_type == 'Independent/Builder Floor':
-    fig1 = px.scatter(new_df[new_df['PROPERTY_TYPE'] == 'Independent/Builder Floor'], x="AREA", y="PRICE", color="BEDROOM_NUM", title="Area Vs Price")
-
-    st.plotly_chart(fig1, use_container_width=True)
-else:
-    fig1 = px.scatter(new_df[new_df['PROPERTY_TYPE'] == 'Independent House/Villa'], x="AREA", y="PRICE", color="BEDROOM_NUM", title="Area Vs Price")
-
-    st.plotly_chart(fig1, use_container_width=True)
+fig1 = px.scatter(new_df[new_df['PROPERTY_TYPE'] == property_type], x="AREA", y="PRICE", color="BEDROOM_NUM", title="Area Vs Price")
+st.plotly_chart(fig1, use_container_width=True)
 
 st.header('BHK Pie Chart')
 
@@ -63,18 +61,18 @@ selected_sector = st.selectbox('Select Sector', sector_options)
 
 if selected_sector == 'overall':
 
-    fig2 = px.pie(new_df, names='bedRoom')
+    fig2 = px.pie(new_df, names='BEDROOM_NUM')
 
     st.plotly_chart(fig2, use_container_width=True)
 else:
 
-    fig2 = px.pie(new_df[new_df['LOCALITY'] == selected_sector], names='bedRoom')
+    fig2 = px.pie(new_df[new_df['LOCALITY'] == selected_sector], names='BEDROOM_NUM')
 
     st.plotly_chart(fig2, use_container_width=True)
 
 st.header('Side by Side BHK price comparison')
 
-fig3 = px.box(new_df[new_df['BEDROOM_NUM'] <= 4], x='bedRoom', y='price', title='BHK Price Range')
+fig3 = px.box(new_df[new_df['BEDROOM_NUM'] <= 4], x='BEDROOM_NUM', y='PRICE', title='BHK Price Range')
 
 st.plotly_chart(fig3, use_container_width=True)
 
@@ -87,3 +85,17 @@ sns.distplot(new_df[new_df['PROPERTY_TYPE'] == 'Independent/Builder Floor']['PRI
 sns.distplot(new_df[new_df['PROPERTY_TYPE'] == 'Independent House/Villa']['PRICE'], label='Independent villa')
 plt.legend()
 st.pyplot(fig3)
+
+st.header('')
+st.header('Most present Featues in an house')
+
+features = ['SWIMMING_POOL', 'POWER_BACKUP', 'CLUB_HOUSE', 'GYM', 'WASTE_DISPOSAL', 'GAS_PIPELINE']
+counts = [new_df[feature].count() for feature in features]
+df = pd.DataFrame({'Feature': features, 'Count': counts})
+sizes = df['Count'] / df['Count'].sum()
+fig, ax = plt.subplots(figsize=(10, 6))
+squarify.plot(sizes=sizes, label=df['Feature'], alpha=0.6, ax=ax)
+ax.axis('off')
+plt.show()
+
+st.pyplot(fig)
